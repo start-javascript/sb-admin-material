4d5a27cc465a6494604f454e4992d3cda6955b0a
Nitin Kumar Mishra <nitin.kr.mr@gmail.com>
2018-06-13 20:54:07 +0530

    minor changes in css

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -26,6 +26,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class DashboardComponent implements OnInit {
     displayedColumns = ['position', 'name', 'weight', 'symbol'];
     dataSource = new MatTableDataSource(ELEMENT_DATA);
+    places: Array<any> = [];
 
     applyFilter(filterValue: string) {
         filterValue = filterValue.trim(); // Remove whitespace
@@ -33,7 +34,34 @@ export class DashboardComponent implements OnInit {
         this.dataSource.filter = filterValue;
     }
 
-    constructor() {}
+    constructor() {
+        this.places = [
+            {
+                imgSrc: 'assets/images/card-1.jpg',
+                place: 'Cozy 5 Stars Apartment',
+                description:
+                    'The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Barcelona.',
+                charge: '$899/night',
+                location: 'Barcelona, Spain'
+            },
+            {
+                imgSrc: 'assets/images/card-2.jpg',
+                place: 'Office Studio',
+                description:
+                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.',
+                charge: '$1,119/night',
+                location: 'London, UK'
+            },
+            {
+                imgSrc: 'assets/images/card-3.jpg',
+                place: 'Beautiful Castle',
+                description:
+                    'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Milan.',
+                charge: '$459/night',
+                location: 'Milan, Italy'
+            }
+        ];
+    }
 
     ngOnInit() {}
 }
