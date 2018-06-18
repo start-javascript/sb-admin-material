e2a4573bc6b7700806e44c8c38407e46b1c77120
Nitin Kumar Mishra <nitin.kr.mr@gmail.com>
2018-06-18 21:18:56 +0530

    submenu implementation in sidebar

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+    showMenu: string = '';
     constructor() {}
 
     ngOnInit() {}
+
+    addExpandClass(element: any) {
+        if (element === this.showMenu) {
+            this.showMenu = '0';
+        } else {
+            this.showMenu = element;
+        }
+    }
 }
