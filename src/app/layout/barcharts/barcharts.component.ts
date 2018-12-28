5820cde8ec0c2f8f2bfa708457c2b1c879760ba0
alpeshrpatel <alpeshrpatel@gmail.com>
2018-12-28 12:36:50 -0700

    clean up

---
+++ src/app/layout/barcharts/barcharts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barcharts',
+  templateUrl: './barcharts.component.html',
+  styleUrls: ['./barcharts.component.scss']
+})
+export class BarchartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/layout/linecharts/linecharts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linecharts',
+  templateUrl: './linecharts.component.html',
+  styleUrls: ['./linecharts.component.scss']
+})
+export class LinechartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
