23b8cdf851d8d73281fa8e35fac37c5dddebe021
alpeshrpatel <alpeshrpatel@gmail.com>
2018-12-28 23:51:49 -0700

    Adding stepper

--- src/app/layout/create-set/create-set.module.ts
+++ src/app/layout/create-set/create-set.module.ts
@@ -3,39 +3,46 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule as FormModule, ReactiveFormsModule } from '@angular/forms';
 import {
-    MatAutocompleteModule, MatCardModule, MatCheckboxModule, MatDatepickerModule,
-    MatFormFieldModule, MatInputModule, MatNativeDateModule, MatRadioModule, MatSelectModule,
-    MatSliderModule, MatSlideToggleModule
+  MatAutocompleteModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatStepperModule,
+  MatButtonModule,
 } from '@angular/material';
-
-
 
 import { CreateSetModuleRoutingModule } from './create-set-routing.module';
 import { CreateSetComponent } from './create-set.component';
 
-
 @NgModule({
-    imports: [
-        CommonModule,
-        CreateSetModuleRoutingModule,
-        MatAutocompleteModule,
-        FormModule,
-        ReactiveFormsModule,
-        MatSlideToggleModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatCardModule,
-        MatCheckboxModule,
-        MatRadioModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        MatSliderModule,
-        FlexLayoutModule.withConfig({addFlexToParent: false})
-    ],
-    declarations: [
-        CreateSetComponent
-    ]
+  imports: [
+    CommonModule,
+    CreateSetModuleRoutingModule,
+    MatAutocompleteModule,
+    FormModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatStepperModule,
+    MatButtonModule,
+    FlexLayoutModule.withConfig({ addFlexToParent: false })
+  ],
+  declarations: [CreateSetComponent]
 })
 export class CreateSetModule {}
