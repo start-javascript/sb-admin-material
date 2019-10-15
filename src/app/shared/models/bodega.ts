1fcf0771136c7760a9a03379ffe696102ef7ddbe
Cristian Izaquita <cristian.izaquita@xphera.co>
2019-10-15 14:34:46 -0500

    Added es.json for i18n

--- src/app/shared/models/bodega.ts
+++
@@ -1,8 +0,0 @@
-export class Bodega {
-    public DIRECCION: string;
-    public BODEGACODIGO: string;
-    public OPA_FONO: string;
-    public OPA_BODEGAPREFIJO: string;
-    public OPA_BODEGAGLOSA: string;
-    public OPA_VIGENTE: number;
-}
